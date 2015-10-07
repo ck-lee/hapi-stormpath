9ea1675f8549578f834044f27f436ca9762c9f7f
chaokoon <chaokoon@gmail.com>
2015-10-07 22:49:30 +1300

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+// Load modules
+
+var Hoek = require('hoek');
+var stormpath = require('stormpath');
+
+
+// Declare internals
+
+var internals = {};
+
+internals.defaults = {};
+
+exports.register = function (server, options, next) {
+
+    Hoek.assert(options.apiKeyId, 'options.apiKeyId is required');
+    Hoek.assert(options.apiKeySecret, 'options.apiKeySecret is required');
+
+    var apiKey = new stormpath.ApiKey(options.apiKeyId, options.apiKeySecret);
+    var client = new stormpath.Client({ apiKey: apiKey });
+
+    internals.client = client;
+
+    if (options.applicationHref) {
+
+        client.getApplication(options.applicationHref, function (err, application) {
+
+            Hoek.assert(!err, err);
+
+            var authenticator = new stormpath.OAuthAuthenticator(application);
+
+            server.expose({
+                application: application,
+                authenticator: authenticator,
+                client: internals.client
+            });
+
+            next();
+        });
+
+    } else {
+
+        server.expose({
+            client: internals.client
+        });
+
+        next();
+    }
+};
+
+exports.register.attributes = {
+    name: 'hapiStormpath',
+    version: '0.0.1'
+};
---
+++ test/index.js
@@ -0,0 +1,34 @@
+// Load modules
+
+var Code = require('code');
+var Hapi = require('hapi');
+var Lab = require('lab');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var lab = exports.lab = Lab.script();
+var it = lab.it;
+var expect = Code.expect;
+
+
+it('errors without API Key', function (done) {
+
+    var server = new Hapi.Server();
+    server.register([{
+        register: require('../'),
+        options:  {
+            apiKeyId: 'test',
+            apiKeySecret: 'test'
+        }
+    }],
+    function (err) {
+
+        expect(err).to.not.exist();
+    });
+});
